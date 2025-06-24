9a1cbda7f81d82581906733e36b6bad1dbdc552c
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-06-24 11:08:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,5 +138,4 @@ def test_api():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)  # Set debug=True for local testing
+    app.run(debug=True)  
